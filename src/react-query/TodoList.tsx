319c2f20482d7e8872efc93419c1426452941a10
KIM YUEN <kim_man@live.com>
2025-10-28 16:17:52 +1100

    first commit

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,6 +1,5 @@
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-
 interface Todo {
   id: number;
   title: string;
@@ -9,21 +8,20 @@ interface Todo {
 }
 
 const TodoList = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [error, setError] = useState('');
+  const fetchTodos =  () => 
+         axios
+          .get<Todo[]>('https://jsonplaceholder.typicode.cosm/todos')
+          .then(res => res.data)
 
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => setTodos(res.data))
-      .catch((error) => setError(error));
-  }, []);
-
-  if (error) return <p>{error}</p>;
-
+  const {data: todos, error, isLoading} = useQuery<Todo[], Error>({
+    queryKey: ['todos'],
+    queryFn: fetchTodos
+  })
+  if(isLoading) return <p>Loading..</p>
+  if(error) return <p>{error.message}</p>
   return (
     <ul className="list-group">
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <li key={todo.id} className="list-group-item">
           {todo.title}
         </li>
