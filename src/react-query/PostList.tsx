a5e1315ae50e02edbdf89ea38a72d79db72547e1
KIM YUEN <kim_man@live.com>
2025-10-28 17:30:34 +1100

    add parameters to queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,18 +1,28 @@
+import { useState } from 'react';
 import usePosts from './usePosts';
 
 const PostList = () => {
-  const {data: posts, error, isLoading} = usePosts();
+  const [userId, setUserId] = useState<number>();
+  const {data: posts, error, isLoading} = usePosts(userId);
 
   if (error) return <p>{error.message}</p>;
   if(isLoading) return <p>Loading.....</p>
   return (
-    <ul className="list-group">
-      {posts?.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <select className="form-select mb-3" onChange={(e)=> setUserId(parseInt(e.target.value))} value={userId}>
+        <option value=""></option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </select>
+      <ul className="list-group">
+        {posts?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
