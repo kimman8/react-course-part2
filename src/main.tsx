5becda3cc488cb1074b3172c662129a7efc07816
KIM YUEN <kim_man@live.com>
2025-10-31 13:08:33 +1100

    mutating data

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,17 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import './index.css';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools/>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import usePosts from './usePosts';
 import React from 'react';
 
