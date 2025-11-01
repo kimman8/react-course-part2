8741f7ffdd4f6f4dfdcfd041e9e118750e724863
KIM YUEN <kim_man@live.com>
2025-11-01 13:45:10 +1100

    refactor: create custom mutation hook for useAddTodo

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,5 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { CACHE_KEY_TODOS } from '../constants';
 
 export interface Todo {
   id: number;
@@ -9,15 +10,15 @@ export interface Todo {
 }
 
 const useTodos = () => {
-    const fetchTodos =  () => 
-        axios
-        .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
-        .then(res => res.data)
-    return useQuery<Todo[], Error>({
-        queryKey: ['todos'],
-        queryFn: fetchTodos,
-        staleTime: 10* 1000
-    })
-}
+  const fetchTodos = () =>
+    axios
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => res.data);
+  return useQuery<Todo[], Error>({
+    queryKey: CACHE_KEY_TODOS,
+    queryFn: fetchTodos,
+    staleTime: 10 * 1000,
+  });
+};
 
 export default useTodos;
